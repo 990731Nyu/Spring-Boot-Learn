d21b52d8d9c8395f2b678833e33cca8e9c56b8c4
yaosun <ys4736@nyu.edu>
2022-08-21 01:21:33 -0400

    try springboot

---
+++ src/test/java/com/ys/Springboot02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/ys/controller/MyController.java
@@ -0,0 +1,4 @@
+package com.ys.controller;
+
+public class MyController {
+}
