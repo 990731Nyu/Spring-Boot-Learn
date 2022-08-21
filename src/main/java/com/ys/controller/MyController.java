79a901fc5d2347e514ede5b8156481da4f027e88
yaosun <ys4736@nyu.edu>
2022-08-21 01:35:14 -0400

    initialize spring boot project

---
+++ src/main/java/com/ys/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.ys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+    @ResponseBody
+    @RequestMapping("/secondController")
+    public String secondController(){
+        return "hello springboot02";
+    }
+}
--- src/test/java/com/ys/controller/MyController.java
+++
@@ -1,4 +0,0 @@
-package com.ys.controller;
-
-public class MyController {
-}
